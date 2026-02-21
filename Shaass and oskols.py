59c89b4f5aeaece985e36e83b0013b0f47fd58d9
Hamza Hussein <hamzahussain5051@gmail.com>
2026-02-21 12:06:49 +0300

    Create Shaass and oskols.py

---
+++ Shaass and oskols.py
@@ -0,0 +1,16 @@
+#flenbar
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+for i in range(m):
+    x, y = map(int, input().split())
+    x -= 1
+    left = y - 1
+    right = a[x] - y
+    if x - 1 >= 0:
+        a[x - 1] += left
+    if x + 1 < n:
+        a[x + 1] += right
+    a[x] = 0
+for i in range(n):
+    print(a[i])
